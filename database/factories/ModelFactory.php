27bb7bbec27e610fdd50672d23c847b8ae788f0e
codjens <jensnieulandt@gmail.com>
2019-01-09 00:06:32 +0100

    Added model factory for Events

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -11,9 +11,26 @@
 |
 */
 
+use Carbon\Carbon;
+
 $factory->define(App\User::class, function (Faker\Generator $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->email,
+    ];
+});
+
+$factory->define(App\Events::class, function (Faker\Generator $faker) {
+    $startTime = Carbon::createFromTimeStamp($faker->dateTimeBetween('now', '+1 month')->getTimestamp());
+    return [
+        'name' => $faker->word,
+        'starts_at' => $startTime,
+        'ends_at' => Carbon::createFromFormat('Y-m-d H:i:s', $startTime)->addHours(2),
+        'status' => 'CONFIRMED',
+        'summary' => $faker->sentence($nbWords = 6, $variableNbWords = true),
+        'location' => $faker->word,
+        'uid' => $faker->domainName,
+        'created_at' => Carbon::now()->toDateTimeString(),
+        'updated_at' => Carbon::now()->toDateTimeString()
     ];
 });
